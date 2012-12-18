59f00f458b384aa653df1c0c98dbd85450acf36b
Timo Schilling <timo@schilling.io>
2012-12-18 04:44:46 +0100

    move recopies in recopies

--- lib/capistrano_ssh.rb
+++
@@ -1,9 +0,0 @@
-require "capistrano/configuration"
-
-Capistrano::Configuration.instance(:must_exist).load do
-  desc "Open a ssh connection to one of the remote servers"
-  task :ssh, :roles => :app do
-    hostname = find_servers_for_task(current_task).first
-    exec "ssh -l #{user} #{hostname} -p #{port} -t 'cd #{current_path} && bash'"
-  end
-end
