7d68083d7ae5cc7f395d17aa0c18e613a31585da
Timo Schilling <timo@schilling.io>
2013-01-17 23:26:52 +0100

    refector recipes for specs

--- lib/capistrano_ssh/recipes.rb
+++ lib/capistrano_ssh/recipes.rb
@@ -1,9 +1,19 @@
 require "capistrano/configuration"
 
-Capistrano::Configuration.instance(:must_exist).load do
-  desc "Open a ssh connection to one of the remote servers"
-  task :ssh, :roles => :app do
-    hostname = find_servers_for_task(current_task).first
-    exec "ssh -l #{user} #{hostname} -p #{port} -t 'cd #{current_path} && bash'"
+module CapistranoSSH
+  module Recipes
+    def self.load_into(configuration)
+      configuration.load do
+        desc "Open a ssh connection to one of the remote servers"
+        task :ssh, :roles => :app do
+          hostname = find_servers_for_task(current_task).first
+          run_locally "ssh -l #{user} #{hostname} -p #{port} -t 'cd #{current_path} && bash'"
+        end
+      end
+    end
   end
+end
+
+if Capistrano::Configuration.instance
+  CapistranoSSH::Recipes.load_into(Capistrano::Configuration.instance)
 end
